7aa87da381879b6af0d17cec5a54f687e4514458
Jeff Verkoeyen <jverkoey@gmail.com>
2009-03-23 00:45:01 -0700

    Implement children rendering and fix the treeview to stay visible always.

--- www/js/snap/ui/treeview.js
+++ www/js/snap/ui/treeview.js
@@ -20,6 +20,8 @@ Snap.TreeView = function(elementIDs, filters) {
   this._filters.register_change_callback(this._filters_changed.bind(this));
 
   this._tree = [];
+  this._tree_ui = [];
+  this._tree_visibility = [];
 
   this._db = Snap.Database.singleton;
 
@@ -35,16 +37,55 @@ Snap.TreeView.prototype = {
   },
 
   _render : function() {
-    var html = [];
     var any_filters = this._filters.any_filtered();
-    // if( !any_filters || this._filters.is_filtered(tree.id) ) {
-    html.push('<ul class="top">');
     for( var i = 0; i < this._tree.length; ++i ) {
       var tree = this._tree[i];
-      html.push('<li>',tree.name,'</li>');
+      if( !any_filters || this._filters.is_filtered(tree.id) ) {
+        if( !this._tree_visibility[i] ) {
+          this._tree_visibility[i] = true;
+          this._tree_ui[i].fadeIn('slow');
+        }
+      } else {
+        if( this._tree_visibility[i] ) {
+          this._tree_visibility[i] = false;
+          this._tree_ui[i].fadeOut('slow');
+        }
+      }
+    }
+  },
+
+  _create_ui : function() {
+    var html = [];
+    html.push('<ul class="top">');
+    function traverse_children(parent_node, className) {
+      var has_children = parent_node.children.length > 0;
+      html.push('<li class="',className,'" style="display:none"><div class="node_text');
+      if( has_children ) {
+        html.push(' has_children');
+      }
+      html.push('">', parent_node.name, '</div>');
+      if( has_children ) {
+        var children = parent_node.children;
+        html.push('<ul>');
+        for( var i = 0; i < children.length; ++i ) {
+          traverse_children(children[i], 'child');
+        }
+        html.push('</ul>');
+      }
+      html.push('</li>');
+    }
+
+    for( var i = 0; i < this._tree.length; ++i ) {
+      var tree = this._tree[i];
+      this._tree_visibility.push(false);
+      traverse_children(tree, 'root');
     }
     html.push('</ul>');
     this._elements.view.html(html.join(''));
+    var t = this;
+    $(this._elementIDs.view+' .root').each(function(index) {
+      t._tree_ui[index] = $(this);
+    });
   },
 
   _receive_hier : function() {
@@ -60,39 +101,47 @@ Snap.TreeView.prototype = {
         var node = list[id];
         // node.name
         // node.ancestors
+
+        // Traverse the ancestry, creating the path along the way.
         var parentnode = null;
         for( var i = 0; i < node.ancestors.length; ++i ) {
-          var parent = node.ancestors[i];
+          var iter = node.ancestors[i];
           if( parentnode ) {
-            if( !parentnode.children[parent] ) {
-              parentnode.children[parent] = {children:{}};
+            if( undefined == parentnode.map[iter] ) {
+              parentnode.children.push({map:{}, children:[]});
+              parentnode.map[iter] = parentnode.children.length - 1;
             }
-            parentnode = parentnode.children[parent];
+            parentnode = parentnode.children[parentnode.map[iter]];
           } else {  
-            if( undefined == map[parent] ) {
-              tree.push({children:{}});
-              map[parent] = tree.length - 1;
+            if( undefined == map[iter] ) {
+              tree.push({map:{}, children:[]});
+              map[iter] = tree.length - 1;
             }
-            parentnode = tree[map[parent]];
+            parentnode = tree[map[iter]];
           }
         }
+
+        // Now that we've created the path, let's add this node.
         if( !parentnode ) {
           if( undefined != map[id] ) {
-            list[map[id]].name = node.name;
+            tree[map[id]].name = node.name;
+            tree[map[id]].id = id;
           } else {
-            tree.push({name:node.name, children:{}});
+            tree.push({name:node.name, id:id, map:{}, children:[]});
             map[id] = tree.length - 1;
           }
         } else {
-          if( undefined != parentnode.children[id] ) {
-            parentnode.children[id].name = node.name;
+          if( undefined != parentnode.map[id] ) {
+            parentnode.children[parentnode.map[id]].name = node.name;
+            parentnode.children[parentnode.map[id]].id = id;
           } else {
-            parentnode.children[id] = {name:node.name, children:{}};
+            parentnode.children.push({name:node.name, id:id, map:{}, children:[]});
+            parentnode.map[id] = parentnode.children.length - 1;
           }
         }
       }
 
-      this._tree.push({id:category, name:this._db.id_to_name(category), tree:tree});
+      this._tree.push({id:category, name:this._db.id_to_name(category), children:tree});
     }
 
     function compare_names(left, right) {
@@ -101,8 +150,23 @@ Snap.TreeView.prototype = {
         (left.name.toLowerCase() == right.name.toLowerCase() ? 0 : 1));
       return result;
     }
+
     this._tree = this._tree.sort(compare_names);
 
+/*    function traverse_tree(node) {
+      for( var i = 0; i < node.children.length; ++i ) {
+        traverse_tree(node.children[i]);
+      }
+      node.children = node.children.sort(compare_names);
+    }
+
+    console.dir(this._tree);
+    for( var i = 0; i < this._tree.length; ++i ) {
+      traverse_tree(this._tree[i]);
+    }
+    console.dir(this._tree);*/
+
+    this._create_ui();
     this._render();
   }
 
